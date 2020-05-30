896b083b0057ef2434a48cbc9007a78aedddce9d
BestStudentSks2019 <Beststudentsks2019@gmail.com>
2020-05-30 17:57:52 +0530

    5th Commit for jenkins

--- SampleTwo/src/main/java/com/example/App.java
+++ SampleTwo/src/main/java/com/example/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Devps with using maven" );
+        
+        System.out.print("Automation via jenkins");
     }
     
     
